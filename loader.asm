95095827663a5c0a863f3a6c415e573b1cd50a14
Duacai <linting0018@hotmail.com>
2018-07-31 20:32:30 +0800

    Lesson 13

--- loader.asm
+++ loader.asm
@@ -4,14 +4,14 @@ org 0x9000
 
 jmp CODE16_SEGMENT
 
-[section .gdt]	; 全局描述符表
+[section .gdt]	; 全局描述符表，部分段基址暂未知地址需使用时再调节
 ; GDT definition
-;									段基址	段界限					段属性
-GDT_ENTRY		:		Descriptor	0,		0,						0
-CODE32_DESC		:		Descriptor	0,		Code32SegLen - 1,		DA_C + DA_32
-VIDEO_DESC		:		Descriptor	0xB8000,0x07FFF,				DA_DRWA + DA_32
-DATA32_DESC		:		Descriptor	0,		Data32SegLen - 1,		DA_DR + DA_32
-STACK_DESC		:		Descriptor	0,		TopOfStackInit,			DA_DRW + DA_32
+;									段基址		段界限					段属性
+GDT_ENTRY		:		Descriptor	0,			0,						0
+CODE32_DESC		:		Descriptor	0,			Code32SegLen - 1,		DA_C + DA_32
+VIDEO_DESC		:		Descriptor	0xB8000,	0x07FFF,				DA_DRWA + DA_32
+DATA32_DESC		:		Descriptor	0,			Data32SegLen - 1,		DA_DR + DA_32
+STACK32_DESC	:		Descriptor	0,			TopOfStack32,			DA_DRW + DA_32
 ; GDT end
 
 GdtLen	equ	$ - GDT_ENTRY
@@ -26,16 +26,16 @@ GdtPtr:					; 全局描述符表指针
 Code32Selector	equ	(0x0001 << 3) + SA_TIG + SA_RPL0	; 0x0001==第二个选择子
 VideoSelector	equ (0x0002 << 3) + SA_TIG + SA_RPL0
 Data32Selector	equ (0x0003 << 3) + SA_TIG + SA_RPL0
-StackSelector	equ (0x0004 << 3) + SA_TIG + SA_RPL0
+Stack32Selector	equ (0x0004 << 3) + SA_TIG + SA_RPL0
 
 ; end of [section .gdt]
 
-TopOfStackInit equ 0x7c00
+TopOfStack16 equ 0x7c00
 
 [section .dat]						; 32位数据段
 [bits 32]
 DATA32_SEGMENT:
-	DTOS				db "D.T.OS!", 0
+	DTOS				db "D.T.OS!", 0			; 注意添加字符串结束标记0
 	DTOS_OFFSET			equ DTOS - $$
 	HELLO_WORLD			db "Hello World!", 0
 	HELLO_WORLD_OFFSET	equ HELLO_WORLD - $$
@@ -49,7 +49,7 @@ CODE16_SEGMENT:
 	mov ds, ax
 	mov es, ax
 	mov ss, ax
-	mov sp, TopOfStackInit
+	mov sp, TopOfStack32
 
 	; initialize GDT for 32 bits code segment
 	mov esi, CODE32_SEGMENT
@@ -59,6 +59,11 @@ CODE16_SEGMENT:
 		
 	mov esi, DATA32_SEGMENT
 	mov edi, DATA32_DESC
+
+	call InitDescItem
+
+	mov esi, STACK32_SEGMENT
+	mov edi, STACK32_DESC
 
 	call InitDescItem
 
@@ -89,19 +94,19 @@ CODE16_SEGMENT:
 	jmp dword Code32Selector : 0	; 使用jmp跳转到32位代码段选择子的0偏移处
 
 
-; esi	--> code  segment label
+; esi	--> code segment label
 ; edi	--> descriptor label
-InitDescItem:						; 初始化描述符
+InitDescItem:						; 初始化描述符项目
 	push eax
 	
 	mov eax, 0						; 代码段地址左移4位
 	mov ax, cs
-	shl eax, 4
+	shl eax, 4						; 实地址=段寄存器地址左移4位+偏移地址
 	add eax, esi
-	mov word [edi + 2], ax			; 
-	shr eax, 16
-	mov byte [edi + 4], al
-	mov byte [edi + 7], ah
+	mov word [edi + 2], ax			; 将段基址写入描述符2个字节（16位寄存器），低32位的16-31bit（偏移2字节）
+	shr eax, 16						; 移除eax实地址中已经写入段基址的2字节数据
+	mov byte [edi + 4], al			; 将段基址写入描述符1个字节(8位寄存器)，高32位的0-7bit（偏移4+0=4字节）
+	mov byte [edi + 7], ah			; 将段基址写入描述符1个字节(8位寄存器)，高32位的24-31bit（偏移4+3=7字节）
 
 	pop eax
 	
@@ -111,18 +116,21 @@ InitDescItem:						; 初始化描述符
 [section .s32]	; 32位代码段
 [bits 32]		; 使用32位编译
 CODE32_SEGMENT:	; 32位代码段数据
-	mov ax, VideoSelector			; 把视频段放到gs寄存器
+	mov ax, VideoSelector			; 把视频段放到gs全局段寄存器
 	mov gs, ax
 
-	mov ax, StackSelector
+	mov ax, Stack32Selector
 	mov ss, ax
+
+	mov eax, TopOfStack32
+	mov esp, eax
 
 	mov ax, Data32Selector
 	mov ds, ax
 
 	mov ebp, DTOS_OFFSET
-	mov bx, 0x0C					; 黑底红字
-	mov dh, 12						; 指定行地址
+	mov bx, 0x0C					; 黑底淡红字
+	mov dh, 12						; 指定行地址，注意行列都是从0开始
 	mov dl, 33						; 指定列地址
 
 	call PrintString
@@ -139,14 +147,14 @@ CODE32_SEGMENT:	; 32位代码段数据
 ; ds:ebp	--> string address
 ; bx		--> attribute
 ; dx		--> dh : row, dl : col
-PrintString:
+PrintString:						; 打印字符串函数
 	push ebp
 	push eax
 	push edi
 	push cx
 	push dx
-print:								; 打印字符串函数
-	mov cl, [ds:ebp]
+print:
+	mov cl, [ds:ebp]				; cl记录要打印的字符
 	cmp cl, 0						; 对比字符串结束符号
 	je end
 	mov eax, 80						; 每行字符数
@@ -154,11 +162,11 @@ print:								; 打印字符串函数
 	add al, dl						; 加上列数，最终计算出要显示的位置
 	shl eax, 1						; 左移乘以2，计算字节偏移
 	mov edi, eax					; 写入显示的偏移地址到edi
-	mov ah, bl						; 字符显示属性
-	mov al, cl						; 字符
+	mov ah, bl						; 字符属性写入高位
+	mov al, cl						; 字符写入低位
 	mov [gs:edi], ax				; 写入显存对应地址
 	inc ebp							; 指向下一个字符
-	inc dl							; 指向下一个显示的位置
+	inc dl							; 指向屏幕的下一列
 	jmp print
 
 end:
@@ -170,3 +178,11 @@ end:
 	ret
 
 Code32SegLen	equ	$ - CODE32_SEGMENT
+
+[section .gs]
+[bits 32]
+STACK32_SEGMENT:
+	times 1024 * 4 db 0
+
+Stack32SegLen equ $ - STACK32_SEGMENT
+TopOfStack32 equ Stack32SegLen - 1
