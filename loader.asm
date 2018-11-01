20145dc641c549b63c6a324a395f4ad1e9c0b8e8
Duacai <linting0018@hotmail.com>
2018-11-01 16:17:32 +0800

    Lesson 20

--- loader.asm
+++ loader.asm
@@ -8,12 +8,17 @@ jmp ENTRY_SEGMENT							; 跳转到ENTRY_SEGMENT入口处
 ; GDT definition		8Byte 64bit
 ;						"函数名"	段基址		段界限					段属性
 GDT_ENTRY		:		Descriptor	0,			0,						0				; 全局段描述符表第0项不使用
-CODE32_DESC		:		Descriptor	0,			Code32SegLen - 1,		DA_C    + DA_32	+ DA_DPL1
+CODE32_DESC		:		Descriptor	0,			Code32SegLen - 1,		DA_C    + DA_32	+ DA_DPL2
 VIDEO_DESC		:		Descriptor	0xB8000,	0x07FFF,				DA_DRWA + DA_32	+ DA_DPL2; 视频段描述符表设置正确无须初始化
-DATA32_DESC		:		Descriptor	0,			Data32SegLen - 1,		DA_DR	+ DA_32	+ DA_DPL2
-STACK32_DESC	:		Descriptor	0,			TopOfStack32,			DA_DRW  + DA_32	+ DA_DPL1
-FUNCTION_DESC	:		Descriptor	0,			FunctionSegLen -1,		DA_C	+ DA_32 + DA_DPL1
-NEW_DESC		:		Descriptor	0,			NewSegLen	 - 1,		DA_CCO	+ DA_32	+ DA_DPL0
+DATA32_DESC_0	:		Descriptor	0,			Data32SegLen0 - 1,		DA_DR	+ DA_32	+ DA_DPL0
+DATA32_DESC_2	:		Descriptor	0,			Data32SegLen2 - 1,		DA_DR	+ DA_32	+ DA_DPL2
+STACK32_DESC_0	:		Descriptor	0,			TopOfStack320,			DA_DRW  + DA_32	+ DA_DPL0
+STACK32_DESC_2	:		Descriptor	0,			TopOfStack322,			DA_DRW  + DA_32	+ DA_DPL2
+TSS_DESC		:		Descriptor	0,			TSSLen	 - 		1,		DA_386TSS		+ DA_DPL0
+FUNCTION_DESC	:		Descriptor	0,			FunctionSegLen -1,		DA_C	+ DA_32 + DA_DPL0
+; Call Gate
+;										选择子				偏移		参数个数			属性
+FUNC_PRINTSTRING_DESC	:	Gate	FunctionSelector,	PrintString,	0,	DA_386CGate + DA_DPL3
 ; GDT end
 
 GdtLen	equ	$ - GDT_ENTRY
@@ -23,18 +28,18 @@ GdtPtr:										; 全局描述符表指针
 		dd	0			; 全局描述符起始地址，先设置为0
 
 
-; GDT Selector		2Byte 16bit
-;								TI：全局、局部	RPL：请求权限级别
-Code32Selector		equ	(0x0001 << 3) + SA_TIG + SA_RPL1	; 0x0001==第二个选择子
+; GDT Selector		2Byte 16bit	TI：全局、局部	RPL：请求权限级别
+Code32Selector		equ	(0x0001 << 3) + SA_TIG + SA_RPL2	; 0x0001==第二个选择子
 VideoSelector		equ (0x0002 << 3) + SA_TIG + SA_RPL2	; 显存特权级低只会影响显示，对系统安全无影响
-Data32Selector		equ (0x0003 << 3) + SA_TIG + SA_RPL2
-Stack32Selector		equ (0x0004 << 3) + SA_TIG + SA_RPL1
-FunctionSelector	equ (0x0005 << 3) + SA_TIG + SA_RPL1
-NewSelector			equ (0x0006 << 3) + SA_TIG + SA_RPL0
-
+Data32Selector0		equ (0x0003 << 3) + SA_TIG + SA_RPL0
+Data32Selector2		equ (0x0004 << 3) + SA_TIG + SA_RPL2
+Stack32Selector0	equ (0x0005 << 3) + SA_TIG + SA_RPL0
+Stack32Selector2	equ (0x0006 << 3) + SA_TIG + SA_RPL2
+TSSSelector			equ (0x0007 << 3) + SA_TIG + SA_RPL0
+FunctionSelector	equ (0x0008 << 3) + SA_TIG + SA_RPL0
+; Gate Selector
+FuncPrintStringSelector	equ	(0x0009 << 3) + SA_TIG + SA_RPL3
 ; end of [section .gdt]
-
-
 
 TopOfStack16 equ 0x7c00
 
@@ -52,27 +57,38 @@ ENTRY_SEGMENT:								; 16位保护模式入口段
 	mov edi, CODE32_DESC
 
 	call InitDescItem
-	
-	mov esi, DATA32_SEGMENT
-	mov edi, DATA32_DESC
+	;  视频段描述符表设置正确无须初始化
+	mov esi, DATA32_SEGMENT_0
+	mov edi, DATA32_DESC_0
 
+	call InitDescItem
+	
+	mov esi, DATA32_SEGMENT_2
+	mov edi, DATA32_DESC_2
+	
 	call InitDescItem
 		
-	mov esi, STACK32_SEGMENT
-	mov edi, STACK32_DESC
+	mov esi, STACK32_SEGMENT_0
+	mov edi, STACK32_DESC_0
 
 	call InitDescItem
-	; 视频段描述符表设在正确无须初始化
-	mov esi, FUNCTION_SEGMENT	; 初始化函数段描述符
+
+	mov esi, STACK32_SEGMENT_2
+	mov edi, STACK32_DESC_2
+
+	call InitDescItem
+	
+	mov esi, FUNCTION_SEGMENT
 	mov edi, FUNCTION_DESC
-	
+
 	call InitDescItem
 	
-	mov esi, NEW_SEGMENT		; 初始化一致性代码段描述符
-	mov edi, NEW_DESC
-	
+	mov esi, TSS_SEGMENT		; 初始化TSS任务段
+	mov edi, TSS_DESC
+
 	call InitDescItem
 
+	
 			; initialize GDT pointer struct
 	mov eax, 0					; 代码段地址左移4位
 	mov ax, ds
@@ -95,14 +111,19 @@ ENTRY_SEGMENT:								; 16位保护模式入口段
 	mov eax, cr0				; 设置cr0寄存器，进入保护模式
 	or eax, 0x01
 	mov cr0, eax
+	
+			; 5. load TSS
+	mov ax, TSSSelector
+	ltr ax
 
-			; 5. jump to 32 bits code
-	push Stack32Selector		;jmp dword Code32Selector : 0 ; 使用jmp跳转到32位代码段选择子的0偏移处
-	push TopOfStack32
+			; 6. jump to 32 bits code
+	push Stack32Selector2		;jmp dword Code32Selector : 0 ; 使用jmp跳转到32位代码段选择子的0偏移处
+	push TopOfStack322
 	push Code32Selector
 	push 0
-	retf						; 弹出2个栈，分别为给IP、CS寄存器
-
+	retf						; 弹出2个栈，分别给IP、CS寄存器
+	
+	
 
 ; esi	--> code segment label
 ; edi	--> descriptor label
@@ -113,7 +134,7 @@ InitDescItem:								; 初始化描述符项目
 	mov ax, cs
 	shl eax, 4						; 实地址=段寄存器地址左移4位+偏移地址
 	add eax, esi
-	mov word [edi + 2], ax			; 将段基址写入描述符2个字节（16位寄存器），低32位的16-31bit（偏移0+2字节）
+	mov word [edi + 2], ax			; 将段基址写入描述符2个字节（16位寄存器），低32位的16-31bit（偏移2字节）
 	shr eax, 16						; 移除eax实地址中已经写入段基址的2字节数据
 	mov byte [edi + 4], al			; 将段基址写入描述符1个字节(8位寄存器)，高32位的0-7bit（偏移4+0=4字节）
 	mov byte [edi + 7], ah			; 将段基址写入描述符1个字节(8位寄存器)，高32位的24-31bit（偏移4+3=7字节）
@@ -122,54 +143,59 @@ InitDescItem:								; 初始化描述符项目
 	
 	ret
 
-[section .dat]
+[section .dat0]
 [bits 32]
-DATA32_SEGMENT:
-	DTOS			db	"D.T.OS!", 0
-	DTOS_OFFSET		equ	DTOS - $$
+DATA32_SEGMENT_0:
+	DPL0			db	"DPL = 0", 0
+	DPL0_OFFSET		equ	DPL0 - $$
 	
-Data32SegLen equ $ - DATA32_SEGMENT
+Data32SegLen0 equ $ - DATA32_SEGMENT_0
 
+[section .dat2]
+[bits 32]
+DATA32_SEGMENT_2:
+	DPL2			db	"DPL = 2", 0
+	DPL2_OFFSET		equ	DPL2 - $$
 	
+Data32SegLen2 equ $ - DATA32_SEGMENT_2
+
+[section .tss]					; TSS任务段104字节，另可附加额外信息
+[bits 32]
+TSS_SEGMENT:
+		dd	0					; 保留前一个TSS段选择子，由CPU填写，高位填0
+		dd	TopOfStack320		; 0特权级栈指针
+		dd	Stack32Selector0	; 0特权级栈段选择子，只能用低2个字节，高位填0
+		dd	0					; 1特权级
+		dd	0					;
+		dd	0					; 2特权级
+		dd	0					;
+		times 4 * 18 dd 0		; 用于切换寄存器的值，由CPU填写，共18个dd类型
+		dw	0					; 最低位为调试陷阱标志T，其余为0
+		dw	$ - TSS_SEGMENT + 2	; I/O Map Base Address
+		db	0xFF				; 结束标记，属于额外信息
+		
+TSSLen	equ	$ - TSS_SEGMENT
+
 [section .s32]	; 32位代码段
 [bits 32]		; 使用32位编译
 CODE32_SEGMENT:								; 32位代码段数据
 	mov ax, VideoSelector			; 把视频段选择子放到gs全局段寄存器
 	mov gs, ax
 
-	mov ax, Data32Selector			; 设置数据段地址
+	mov ax, Data32Selector2			; 设置数据段地址
 	mov ds, ax
 	
-	mov ax, Stack32Selector			; 设置栈段地址
-	mov ss, ax
-
-	mov eax, TopOfStack32			; 设置32位栈顶地址
-	mov esp, eax
-
-	;mov ebp, DTOS_OFFSET			; 全局函数打印字符串，使用选择子：偏移量调用
-	;mov bx, 0x0c
-	;mov dh, 12
-	;mov dl, 33
-	
-	;call FunctionSelector : PrintString
-	
-	jmp NewSelector : 0 
-
-Code32SegLen	equ		$ - CODE32_SEGMENT
-
-[section .new]	; conforming code seg
-[bits 32]
-NEW_SEGMENT:
-	mov ebp, DTOS_OFFSET			; 一致性代码段打印字符串，使用选择子：偏移量调用
+	mov ebp, DPL2_OFFSET			; 全局函数打印字符串，使用选择子：偏移量调用
 	mov bx, 0x0c
 	mov dh, 12
 	mov dl, 33
 	
-	call FunctionSelector : PrintString
-
+	call FuncPrintStringSelector : 0
+	
 	jmp $
 
-NewSegLen	equ	$ - NEW_SEGMENT
+Code32SegLen	equ		$ - CODE32_SEGMENT
+
 
 [section .func]
 [bits 32]
@@ -186,23 +212,23 @@ PrintStringFunc:
 	push dx
 	
 Print:
-	mov cl, [ds:ebp]		; 取一个字符
-	cmp cl, 0				; 结束符判断
+	mov cl, [ds:ebp]
+	cmp cl, 0
 	je end
-	mov eax, 80				; 每行字符列数
-	mul dh					; 乘以行数
+	mov eax, 80
+	mul dh
     ;add al, dl             ; 总偏移字符数，al太小超过255会溢出（3*80+16==3行16列）
     push dx                 ; 备份dx（dh），供循环调用避免丢失行号
     mov dh, 0               ; dx高位置0，确保dx=dl
     add ax, dx              ; 总偏移字符数（65536个字符）
     pop dx
-	shl eax, 1				; 每个字符占2字节
+	shl eax, 1
 	mov edi, eax
-	mov ah, bl				; 字符属性
-	mov al, cl				; 字符
-	mov [gs:edi], ax		; 写入显存
-	inc ebp					; 字符源地址递增
-	inc dl					; 显存字符地址递增
+	mov ah, bl
+	mov al, cl
+	mov [gs:edi], ax
+	inc ebp
+	inc dl
 	jmp Print
 	
 end:
@@ -211,18 +237,29 @@ end:
 	pop edi
 	pop eax
 	pop ebp
+	
+	mov ax, Data32Selector0
+	mov ds, ax
+	mov gs, ax
 
 	retf
 	
 PrintString		equ PrintStringFunc - $$
 
 FunctionSegLen	equ $ - FUNCTION_SEGMENT
-
-[section .gs]
+	
+[section .gs0]
 [bits 32]
-STACK32_SEGMENT:
+STACK32_SEGMENT_0:							; 32位栈段定义
 	times 1024 * 4 db 0
 
-Stack32SegLen equ $ - STACK32_SEGMENT
-TopOfStack32 equ Stack32SegLen - 1
+Stack32SegLen0 equ $ - STACK32_SEGMENT_0
+TopOfStack320 equ Stack32SegLen0 - 1
 
+[section .gs2]
+[bits 32]
+STACK32_SEGMENT_2:							; 32位栈段定义
+	times 1024 * 4 db 0
+
+Stack32SegLen2 equ $ - STACK32_SEGMENT_2
+TopOfStack322 equ Stack32SegLen2 - 1
